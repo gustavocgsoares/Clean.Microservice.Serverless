8800ca386c11d478468e1ea758682ad9d21c82a4
Gustavo Cesar Gonçalves Soares <gu.cgsoares@gmail.com>
2019-07-18 10:58:56 -0300

    Add files via upload

---
+++ src/Plugin.Database.SqlServer/SqlServerDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Clean.Microservice.Serverless.Plugin.Database.SqlServer
+{
+    public class SqlServerDatabase : IDatabase
+    {
+        private string connectionString;
+
+        public virtual IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public virtual void Configure(
+            List<DatabaseSettings> databaseSettings,
+            string databaseConnectionName)
+        {
+            var connectionString = databaseSettings?
+                .FirstOrDefault(ds => (ds.SettingsKey?
+                    .Equals(databaseConnectionName, StringComparison.InvariantCultureIgnoreCase))
+                    .GetValueOrDefault())?.ConnectionString;
+
+            if (!string.IsNullOrWhiteSpace(connectionString))
+            {
+                this.connectionString = connectionString;
+            }
+        }
+    }
+}
